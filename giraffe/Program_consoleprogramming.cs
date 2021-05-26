d90ab9540c5495fef987eb2d34119b2affd26155
iposh-hub <82669904+iposh-hub@users.noreply.github.com>
2021-05-26 17:45:28 +0300

    Add files via upload

---
+++ giraffe/Program_consoleprogramming.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Giraffe2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string UserName, UserSurname, UserEmail;
+            Console.Write("Please enter your name: ");
+            UserName = Console.ReadLine();
+            Console.Write("Please enter your surname: ");
+            UserSurname= Console.ReadLine();
+            Console.Write("Please enter your email: ");
+            UserEmail = Console.ReadLine();
+
+
+            Console.WriteLine("You successfully logged in with  " + UserEmail);
+            Console.WriteLine(UserName +" " + UserSurname + " Welcome! ");
+        }
+    }
+}
